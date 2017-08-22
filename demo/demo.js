4be1588384a173afce5c1892275a2364db045782
Aris <afrisy@126.com>
2017-08-23 06:30:19 +0800

    添加gitignore editorConfig

--- demo/demo.js
+++ demo/demo.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(
-    <h1>老婆美腻不可方物~O(∩_∩)O~</h1>,
+    <h1>老婆娴静淑雅，美腻不可方物~O(∩_∩)O~</h1>,
     document.getElementById('root')
 );
